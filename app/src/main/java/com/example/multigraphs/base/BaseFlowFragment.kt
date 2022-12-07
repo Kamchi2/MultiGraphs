219064ca31981320391ee43f3047e12165dc9ef6
KamchiDjunusov <dimooooon70@gmail.com>
2022-12-07 19:04:17 +0600

    Initial Commit

--- app/src/main/java/com/example/multigraphs/base/BaseFlowFragment.kt
+++ app/src/main/java/com/example/multigraphs/base/BaseFlowFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.annotation.IdRes
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 
 abstract class BaseFlowFragment(
@@ -14,10 +15,13 @@ abstract class BaseFlowFragment(
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val navHostFragment =
             childFragmentManager.findFragmentById(idNavHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
+        setupNavigation(navController)
+    }
+
+    protected open fun setupNavigation(navController: NavController) {
 
     }
 }
